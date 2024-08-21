f9727b2104133ea2def7b78395a209b7b7778d59
Will Haltom <willhalto@github.com>
2024-08-21 14:35:27 +0000

    parse statements, meta commands

--- repl.go
+++ repl.go
@@ -13,10 +13,58 @@ func main() {
 		fmt.Print("db> ")
 		text, _ := reader.ReadString('\n')
 		command := strings.TrimSpace(text)
-		if command == "exit" {
-			return
-		} else {
-			fmt.Printf("Unrecognized command: %s\n", command)
+
+		if strings.HasPrefix(command, ".") {
+			err := doMetaCommand(command)
+			if err != nil {
+				fmt.Printf("Error: %s\n", err)
+			}
+
+			continue
 		}
+
+		statement, err := prepareStatement(command)
+		if err != nil {
+			fmt.Printf("Error: %s\n", err)
+			continue
+		}
+
+		execute_statement(statement)
+	}
+}
+
+const (
+	STATEMENT_INSERT string = "statement_insert"
+	STATEMENT_SELECT string = "statement_select"
+)
+
+type Statement struct {
+	Type string
+}
+
+func doMetaCommand(command string) error {
+	if command == ".exit" {
+		os.Exit(0)
+	}
+	return fmt.Errorf("unrecognized commmand: %s", command)
+}
+
+func prepareStatement(command string) (Statement, error) {
+	switch command {
+	case "insert":
+		return Statement{Type: STATEMENT_INSERT}, nil
+	case "select":
+		return Statement{Type: STATEMENT_SELECT}, nil
+	default:
+		return Statement{}, fmt.Errorf("unrecognized statment: %s", command)
+	}
+}
+
+func execute_statement(statement Statement) {
+	switch statement.Type {
+	case STATEMENT_INSERT:
+		fmt.Println("This would be an insert.")
+	case STATEMENT_SELECT:
+		fmt.Println("This would be a select.")
 	}
 }
